7a943e5e56d36131031056be20b2b217d199cf21
Alejandro Sherwell <sherwell0@gmail.com>
2016-12-08 16:16:26 -0600

    Adds update action to the users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 		end
 
 		describe "POST #create" do 
-			context "when is successfully created" do 
+			context "with valid attributes" do 
 				before(:each) do 
 					@user_attributes = attributes_for(:user)
 					post :create, user: @user_attributes, format: :json
@@ -35,7 +35,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 				end
 			end
 
-			context "when is not created" do 
+			context "with invalid attributes" do 
 				before(:each) do 
 					#Not including email
 					@invalid_user_attributes = { password: "12345678", password_confirmation: "12345678"}
@@ -57,6 +57,43 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 					expect(response.status).to eq(422)
 				end
 			end
+		end
+
+		describe "PUT/PATCH #update" do 
+			context "with valid attributes" do 
+				before(:each) do 
+					patch :update, { id: subject.id, user: { email: "newmail@example.com" } }, format: :json
+				end
+
+				it "renders the json representation for the updated user" do 
+					user_response = JSON.parse(response.body, symbolize_names: true)
+					expect(user_response[:email]).to eql("newmail@example.com")
+				end
+
+				it "respons with status-code 200" do 
+					expect(response.status).to eq(200)
+				end
+			end
+
+			context "with invalid attributes" do 
+				before(:each) do 
+					patch :update, { id: subject.id, user: { email: "bademail.com" } }
+				end
+
+				it "renders an errors json" do 
+					user_response = JSON.parse(response.body, symbolize_names: true)
+					expect(user_response).to have_key(:errors)
+				end
+
+				it "renders the json errors on why the user could not be created" do 
+					user_response = JSON.parse(response.body, symbolize_names: true)
+					expect(user_response[:errors][:email]).to include "is invalid"
+				end
+
+				it "responds with status-code 422" do 
+					expect(response.status).to eq(422)
+				end
+			end	
 		end
 	end
 end
