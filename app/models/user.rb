2b51a52370103ce61583f214b3f4cb4f216d4d94
Alejandro Sherwell <sherwell0@gmail.com>
2016-12-10 14:46:51 -0600

    Adds sessions controller create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,9 @@ class User < ActiveRecord::Base
 
   before_create :generate_authentication_token!
 
-  private
-  	def generate_authentication_token! 
-  		begin
-  			self.auth_token = Devise.friendly_token
-  		end while self.class.exists?(auth_token: auth_token)
-  	end
+	def generate_authentication_token! 
+		begin
+			self.auth_token = Devise.friendly_token
+		end while self.class.exists?(auth_token: auth_token)
+	end
 end
---
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+RSpec.describe Api::V1::SessionsController, type: :controller do
+	describe "POST #create" do 
+		context "with valid credentials" do 
+			let(:subject){ create :user }
+			before(:each) do 
+				credentials = { email: subject.email, password: "12345678" }
+				post :create, { session: credentials }
+			end
+
+			it "returns the user record corresponding to the given credentials" do 
+				subject.reload
+				expect(json_response[:auth_token]).to eql(subject.auth_token)
+			end
+
+			it "returns status code 200" do 
+				expect(response.status).to eq(200)
+			end
+		end
+
+		context "with invalid credentials" do 
+			let(:subject){ create :user }
+			before(:each) do 
+				credentials = { email: subject.email, password: "invalidpassword" }
+				post :create, { session: credentials }
+			end
+
+			it "returns a json with an error" do 
+				expect(json_response[:errors]).to eql("Invalid email or password")
+			end
+
+			it "returns status code 422" do 
+				expect(response.status).to eq(422)
+			end
+		end
+	end
+end
